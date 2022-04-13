10d023c9ffe7d2fc6379c9bce17b2a35809d0959
EricTierre <53849601+EricTierre@users.noreply.github.com>
2022-04-13 18:45:58 -0400

    test

--- app.py
+++ app.py
@@ -2,29 +2,50 @@ from flask import Flask, render_template, jsonify
 from flask_restful import Resource, Api
 from flask_pymongo import PyMongo
 
+
+# def create_app():
+#     app = Flask(__name__)
+#     #Session
+#     app.config["SECRET_KEY"] = "harrypotter"
+
+#     #Connect with the MongoDB
+#     app.config["MONGO_URI"] = "mongodb+srv://Eric:15975321@pokecrud.n8qha.mongodb.net/POKECRUD?retryWrites=true&w=majority"
+#     from extensions import database
+#     database.init_app(app)
+
+#     #Organize the routes
+#     from routes.main import main
+#     app.register_blueprint(main)
+    
+#     from routes.users import user
+#     app.register_blueprint(user)
+
+#     from routes.pokemon import pokemon
+#     app.register_blueprint(pokemon)
+
+#     return app
+
 app = Flask(__name__)
-def create_app():
+#Session
+app.config["SECRET_KEY"] = "harrypotter"
+
+#Connect with the MongoDB
+app.config["MONGO_URI"] = "mongodb+srv://Eric:15975321@pokecrud.n8qha.mongodb.net/POKECRUD?retryWrites=true&w=majority"
+from extensions import database
+database.init_app(app)
+
+#Organize the routes
+from routes.main import main
+app.register_blueprint(main)
     
-    #Session
-    app.config["SECRET_KEY"] = "harrypotter"
+from routes.users import user
+app.register_blueprint(user)
 
-    #Connect with the MongoDB
-    app.config["MONGO_URI"] = "mongodb+srv://Eric:15975321@pokecrud.n8qha.mongodb.net/POKECRUD?retryWrites=true&w=majority"
-    from extensions import database
-    database.init_app(app)
-
-    #Organize the routes
-    from routes.main import main
-    app.register_blueprint(main)
-    
-    from routes.users import user
-    app.register_blueprint(user)
-
-    from routes.pokemon import pokemon
-    app.register_blueprint(pokemon)
-
-    return app
+from routes.pokemon import pokemon
+app.register_blueprint(pokemon)
 
 if __name__ == "__main__":
-    app = create_app()
-    app.run(debug=True)
+    # app = create_app()
+    # app.run(debug=True)
+
+    app.run()
